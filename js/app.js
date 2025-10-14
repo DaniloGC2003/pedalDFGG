98c33704b1f0dca24cc734d7d97ac253cedcec25
DaniloGC2003 <chinendanilo@gmail.com>
2025-10-14 18:31:12 -0300

    added buttons for BLE device control

--- js/app.js
+++ js/app.js
@@ -1,11 +1,58 @@
 const connectBTN = document.querySelector(".connect");
+const disconnectBTN = document.querySelector(".disconnect");
+const sendBTN = document.querySelector(".send");
 let device;
+let messageCharacteristic;
 // Register the service worker
 /*if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/sw.js')
     .then((reg) => console.log('SW registered', reg))
     .catch((error) => console.error('SW registration failed', error));
 }*/
+
+
+function handleRateChange(event) {
+  console.log("mensagem recebida\n");
+  console.log(event.target.value);
+}
+
+
+async function connectDevice() {
+    console.log("connecting...");
+    if (device.gatt.connected) return;
+
+    const server = await device.gatt.connect();
+    const service = await server.getPrimaryService("4fafc201-1fb5-459e-8fcc-c5c9c331914b");
+
+    messageCharacteristic = await service.getCharacteristic("beb5483e-36e1-4688-b7f5-ea07361b26a8");
+    messageCharacteristic.addEventListener("characteristicvaluechanged", handleRateChange);
+    disconnectBTN.classList.remove("hide");
+    connectBTN.classList.add("hide");
+    console.log("connected");
+}
+
+async function disconnectDevice() {
+    // Verificar se o dispositivo foi definido e se ele está conectado
+    if (device && device.gatt.connected) {
+        console.log("Disconnecting...");
+        
+        device.gatt.disconnect();
+        
+        console.log("BLE device disconnected");
+        device = null;
+        
+        if (connectBTN) {
+            connectBTN.textContent = "connect";
+        }
+
+    } else if (device) {
+        console.log("Device already disconnected.");
+    } else {
+        console.log("No device selected for disconnection.");
+    }
+    disconnectBTN.classList.add("hide");
+    connectBTN.classList.remove("hide");
+}
 
 async function requestDevice() {
     const options = {
@@ -14,10 +61,33 @@ async function requestDevice() {
     };
     device = await navigator.bluetooth.requestDevice(options);
 
-    device.addEventListener("gattserverdisconnected", connectDevice);
+    //device.addEventListener("gattserverdisconnected", connectDevice);
 }
+
+async function startMonitoring() {
+    await messageCharacteristic.startNotifications();
+    //heartUI.classList.remove("pause-animation");
+}
+
+
+
+async function sendData(data) {
+    if (!messageCharacteristic) {
+        console.error("Characteristic not intialized.");
+        return;
+    }
+    
+    const dataBuffer = new TextEncoder().encode(data); 
+
+    console.log(`Sending data: ${data}`);
+
+    await messageCharacteristic.writeValue(dataBuffer);
+
+    console.log("Data sent successfully.");
+}
+
+
 async function init() {
-    console.log("init function")
     if (!navigator.bluetooth) return errorTxt.classList.remove("hide");
 
     if (!device) await requestDevice();
@@ -26,4 +96,7 @@ async function init() {
     await startMonitoring();
     //appUI.classList.remove("hide"); 
 }
+
 connectBTN.addEventListener("click", init);
+disconnectBTN.addEventListener("click", disconnectDevice);
+sendBTN.addEventListener("click", sendData.bind(null, 5));
