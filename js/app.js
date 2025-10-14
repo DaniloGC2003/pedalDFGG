d026adaacae20fa341c2f15ef4cb5e031821dd8d
DaniloGC2003 <chinendanilo@gmail.com>
2025-10-13 21:56:19 -0300

    added BLE functions/removed service worker

--- js/app.js
+++ js/app.js
@@ -1,6 +1,29 @@
+const connectBTN = document.querySelector(".connect");
+let device;
 // Register the service worker
-if ('serviceWorker' in navigator) {
+/*if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/sw.js')
     .then((reg) => console.log('SW registered', reg))
     .catch((error) => console.error('SW registration failed', error));
+}*/
+
+async function requestDevice() {
+    const options = {
+    acceptAllDevices: true,
+    optionalServices: ["4fafc201-1fb5-459e-8fcc-c5c9c331914b"],
+    };
+    device = await navigator.bluetooth.requestDevice(options);
+
+    device.addEventListener("gattserverdisconnected", connectDevice);
 }
+async function init() {
+    console.log("init function")
+    if (!navigator.bluetooth) return errorTxt.classList.remove("hide");
+
+    if (!device) await requestDevice();
+    connectBTN.textContent = "connecting...";
+    await connectDevice();
+    await startMonitoring();
+    //appUI.classList.remove("hide"); 
+}
+connectBTN.addEventListener("click", init);
